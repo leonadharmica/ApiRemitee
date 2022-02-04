4a7005ab6b1958206a4be539e372d06d7c328d2e
Jessica Gherscovic <jessicagherscovic@gmail.com>
2022-02-04 13:45:28 -0300

    Agregar archivos de proyecto.

---
+++ ApiRemitee/ApiRemitee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ApiRemitee
+{
+    public class ApiRemitee
+    {
+        public double Monto { get; set; }
+        public string Moneda { get; set; }
+
+        public double Fee { get; set; }
+        public double MontoDeCambio { get; set; }
+    }
+}
---
+++ ApiRemitee/Controllers/ApiRemiteeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRemitee.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiRemiteeController : ControllerBase
+    { 
+
+        private readonly ILogger<ApiRemiteeController> _logger;
+
+        public ApiRemiteeController(ILogger<ApiRemiteeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ApiRemitee> Get()
+        {
+           
+        }
+    }
+}
